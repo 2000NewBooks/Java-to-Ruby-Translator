88ebca96b8997602c64562bf1c65f3b460a643dc
Andrew Kitterman <taupitheta@gmail.com>
2016-05-23 17:27:32 -0600

    That darn constructor

--- Translator.java
+++ Translator.java
@@ -10,13 +10,7 @@ public class Translator
     private static ArrayList<Variable> vars = new ArrayList<Variable>(); //Stores variables to be added as state variables in the translated code
     private static int lineNum = -1; //Stores which line is being read
 
-    /**
-     * Constructor for objects of class Translator (Unused)
-     */
-    public Translator()
-    {
-
-    }
+    
 
     /**
      * Takes the user's Ruby file, then translates it to java and prints it to a new file
@@ -29,7 +23,7 @@ public class Translator
         Scanner kbReader = new Scanner(System.in);
         System.out.println("Please input the path of the file you would ike to have translated");
         String fileName = kbReader.nextLine();
-        System.out.println("Please input the name of the translated file to be output");
+        System.out.println("Please input the name of the translated file to be output \n ");
         String exportName = kbReader.nextLine();
         File file = null;
         FileWriter writer = null;
@@ -82,7 +76,7 @@ public class Translator
         }
         try
         {
-            writer.flush();
+            
             writer.close();
         }
         catch(IOException I)
@@ -112,20 +106,21 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("while")+5, eqPos ), "double");
             vars.add(var);
+            System.out.println("while (");
         }
         else if (line.indexOf("do") != -1)
         {
             newLine += "";
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("do") + 2, eqPos), "double");
-            vars.add(var);
+            vars.add(var);System.out.println("do");
         }
         else if (line.indexOf("for") != -1)
         {
             newLine += "for (";
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("for") + 3, eqPos), "double");
-            vars.add(var);
+            vars.add(var);System.out.println("for (");
         }
         else if(line.indexOf("until") != -1 )
         {
@@ -136,9 +131,11 @@ public class Translator
                 eqPos = line.trim().indexOf("="); //Works sorta
                 Variable var = new Variable(line.substring(line.indexOf("until") + 5 , eqPos), "double");
                 vars.add(var);
+                System.out.println("do{} while");
             }
+            //C:/Users/PiMaster314/Documents/GitHub/Java-to-Ruby-Translator/2KittermanAndrewProject20%/.rb
         }
-
+        
         /* CONSOLE */
 
         else if(line.indexOf("puts") != -1)
@@ -147,6 +144,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("puts") + 4 , eqPos), "String");
             vars.add(var);
+            System.out.println("System.out.println(//words);");
         }
 
         else if(line.indexOf(".chomp") != -1)
@@ -155,6 +153,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf(".chomp") + 6 , eqPos), "String");
             vars.add(var);
+            System.out.println("Console console = System.console(); String line = console.readLine(//words you want to take input from) ");
         }
 
         /* VARIABLES (Unfinished) */
@@ -165,14 +164,16 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("Array.new") + 9 , eqPos), "double");
             vars.add(var);
+            System.out.println("double[] newArr = new double[some number here];");
         }
 
         else if(line.indexOf("Array.each") != -1)
         {
-            newLine += "for(double u; newArr); {/*Some expression here/*}";
+            newLine += "for(int u; newArr); {/*Some expression here/*}";
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("Array.each") + 10 , eqPos), "double");
             vars.add(var);
+            System.out.println("for(double u; newArr); {/*Some expression here/*}");
         }
 
         /* OPERATIONS */
@@ -180,26 +181,31 @@ public class Translator
         else if(line.indexOf("+") != -1)
         {
             newLine = line + ";";
+            System.out.println("+");
         }
 
         else if(line.indexOf("-") != -1)
         {
             newLine = line + ";";
+            System.out.println("-");
         }
 
         else if(line.indexOf("/") != -1)
         {
             newLine = line + ";";
+            System.out.println("/");
         }
 
         else if(line.indexOf("*") != -1)
         {
             newLine = line + ";";
+            System.out.println("*");
         }
 
         else if(line.indexOf("%") != -1)
         {
             newLine = line + ";";
+            System.out.println("%");
         }
         /* I fixed all the variable assignment 4/25/16 -Andrew */
 
@@ -209,6 +215,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("$") + 1 , eqPos), "String");
             vars.add(var);
+            System.out.println("public double");
         }
         else if(line.indexOf("@")!= -1)
         {
@@ -216,6 +223,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("@") + 1 , eqPos), "String");
             vars.add(var);
+            System.out.println("protected double");
         }
         else if(line.indexOf("@@")!= -1)
         {
@@ -223,6 +231,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("@@") + 1 , eqPos), "String");
             vars.add(var);
+            System.out.println("private double");
         }
         else if(line.indexOf("def")!= -1)
         {
@@ -230,7 +239,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("def") +1, eqPos), "String");
             vars.add(var);
-
+            System.out.println("public double mystery(){}");
         }
         else if(line.indexOf("end")!= -1)
         {
@@ -238,7 +247,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("end") +1, eqPos), "String");
             vars.add(var);
-
+            System.out.println("return;");
         }
         else if(line.indexOf("return")!= -1)
         {
@@ -246,7 +255,7 @@ public class Translator
             eqPos = line.trim().indexOf("=");
             Variable var = new Variable(line.substring(line.indexOf("return") +1, eqPos), "String");
             vars.add(var);
-
+            System.out.println("return double d;");
         }//so basically we don't want to take the time to translate all this crap. That would take years.
         //so we're going to make sure it can recognize it at least
         //I don't even know what half of this is - Kitterman
